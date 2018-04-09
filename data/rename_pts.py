69ce97ef844341fd810a85ac7ee76a6b6961011a
IsaacGuan <yanranguan1994@gmail.com>
2018-04-09 18:52:23 -0400

    prepare .pts data

---
+++ data/rename_pts.py
@@ -0,0 +1,15 @@
+import os
+
+os.chdir("./points")
+
+files = os.listdir('.')
+files.sort()
+
+i = 1
+
+for file in files:
+	if file[-2: ] == 'py':
+		continue
+	new_name = "table" + str(i) + ".pts"
+	i += 1
+	os.rename(file, new_name)
